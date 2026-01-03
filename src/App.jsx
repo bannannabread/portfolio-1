d60d524edec12a38c4e98728f6adc4c85a617a7e
bannannabread <mjacob46@illinois.edu>
2026-01-03 00:59:09 -0600

    Created beginning of Navbar and landing page (Introduction).

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,31 @@ function App() {
 
   return (
     <div>
+      <div class='NavBar'>
+        <ul class='NavItems'>
+          <li>Megan Jacob</li>
+          <li>About</li>
+          <li>Experience</li>
+          <li>Projects</li>
+          <li>Blog</li>
+          <li>Contact</li>
+        </ul>
+      </div>
+
+      <div className='Intro'>
+        <h1>Hi, my name is Megan Jacob</h1>
+        <h2>I am an aspiring Frontend Developer</h2>
+        <p>A passionate developer with expertise 
+          in Next.js and React. Currently pursuing
+          a bachelors in Computer Science and a
+          minor in Informatics at the University 
+          of Illinois Urbana-Champaign.
+        </p>
+      </div>
+
 
     </div>
+
   )
 }
 
